f5751a6d6dcb0021a1cddecb3cc875c38bce6ee8
imraan011 <ishtikharkhandl@gmail.com>
2025-12-14 17:21:22 +0530

    Day 30 | Commit 6

--- Exercise22/basic form/src/App.jsx
+++ Exercise22/basic form/src/App.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
-
+import React, { useState } from "react";
 
 const App = () => {
-  
-  return (
-    <div>App</div>
-  )
-}
+  // const [task, settask] = useState("");
+  // const [todo, settodo] = useState([
+  //   {
+  //     title: "Learn React",
+  //     status: false,
+  //   },
+  //   {
+  //     title: "Learn JavaScript",
+  //     status: false,
+  //   },
+  // ]);
+  const [done, setdone] = useState(true);
+  const [title, settitle] = useState("");
+  const [gender, setgender] = useState("female");
 
-export default App
+  return (
+    <div>
+      <h1>TO DO LIST</h1>
+      <form action="">
+        <input
+          type="text"
+          placeholder="title"
+          onChange={(e) => {
+            settitle(e.target.value);
+          }}
+        />{" "}
+        <br />
+        <br />
+        <input
+          checked={done}
+          type="checkbox"
+          name=""
+          id=""
+          onChange={(e) => {
+            setdone(e.target.checked);
+          }}
+        />
+        Completed <br /> <br />
+        <input
+          type="radio"
+          name=""
+          id=""
+          onChange={(e) => {
+            setgender(e.target.value);
+          }}
+          checked={gender == "male" && true}
+        />{" "}
+        Male
+        <input
+          type="radio"
+          name=""
+          id=""
+          onChange={(e) => setgender(e.target.value)}
+          checked={gender == "female" && true}
+        />
+        Female <br />
+        <br />
+        <button>CREATE TODO</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
