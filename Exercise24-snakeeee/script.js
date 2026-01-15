04a94446d09c0ff1d4cc0e31869b45492f000383
imraan011 <ishtikharkhandl@gmail.com>
2026-01-15 14:14:11 +0530

    Day 38 | Commit 6

--- Exercise24-snakeeee/script.js
+++ Exercise24-snakeeee/script.js
@@ -1,12 +1,44 @@
-const board = document.querySelector(".board")
-const box_hight = 30;
-const box_width = 30;
+const board = document.querySelector(".board");
+const box_hight = 50;
+const box_width = 50;
 
-const colomns = Math.floor(board.clientWidth / box_width)
-const rows = Math.floor(board.clientHeight / box_hight)
+const colomns = Math.floor(board.clientWidth / box_width);
+const rows = Math.floor(board.clientHeight / box_hight);
 
-for(let i =1 ; i<= colomns * rows ; i++){
+const boardArray = [];
+snake = [
+  {
+    x: 2,
+    y: 4,
+  },
+  {
+    x: 2,
+    y: 5,
+  },
+  {
+    x: 2,
+    y: 6,
+  },
+];
+
+const directions = "right";
+
+for (let i = 0; i < rows; i++) {
+  for (let j = 0; j < colomns; j++) {
     const box = document.createElement("div");
     box.classList.add("box");
     board.appendChild(box);
+    box.innerHTML = `${i},${j}`;
+    boardArray[`${i},${j}`] = box;
+  }
 }
+
+function render() {
+  snake.forEach((element) => {
+    boardArray[`${element.x},${element.y}`].classList.add("filled");
+  });
+}
+
+setInterval(() => {
+  render();
+}, 300);
