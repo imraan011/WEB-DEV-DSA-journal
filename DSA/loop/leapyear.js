1737cb636aeafbea607d866c53148cf26550d4ae
imraan011 <ishtikharkhandl@gmail.com>
2026-01-16 22:16:54 +0530

    Day 39 | Commit 1 - leap year

---
+++ DSA/loop/leapyear.js
@@ -0,0 +1,9 @@
+function leap(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  } 
+    
+}
+console.log(leap(2020)); // true
