0cd328f4dba6fc8212884ba4375802be213e5fe0
imraan011 <ishtikharkhandl@gmail.com>
2026-01-20 11:24:27 +0530

    Day 42 | Commit 1 - base pattern

---
+++ DSA/pattern/Basepattern.js
@@ -0,0 +1,12 @@
+var prompt = require("prompt-sync")();
+function Basepattern() {
+  const n = Number(prompt("Enter the Number ... "));
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      process.stdout.write("* ");
+    }
+    console.log();
+  }
+}
+
+Basepattern();
