cfe4e87530bac8250232e83437c7962d4f1c0a79
imraan011 <ishtikharkhandl@gmail.com>
2026-01-20 20:44:07 +0530

    Day 42 | Commit 9

---
+++ DSA/pattern/right_sided_triangle.js
@@ -0,0 +1,25 @@
+//         *
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+
+var prompt = require("prompt-sync")();
+function right_rightanglepattern() {
+  const n = 5;
+  // const n = Number(prompt("Enter the Number ... "));
+  for (let i = 1; i <= n; i++) {
+    // outer loop for rows
+    for (let j = 1; j <= n - i; j++) {
+      // inner loop for columns
+      process.stdout.write("  ");
+    }
+    for (let k = 1; k <= i; k++) {
+      process.stdout.write("* ");
+    }
+
+    console.log();
+  }
+}
+
+right_rightanglepattern();
