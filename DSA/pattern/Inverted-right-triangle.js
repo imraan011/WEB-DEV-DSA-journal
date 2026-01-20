1980f6fc11984e3b9d11a49bf289237b1bbfc240
imraan011 <ishtikharkhandl@gmail.com>
2026-01-20 12:22:17 +0530

    Day 42 | Commit 7 - innverted right angle pattern

---
+++ DSA/pattern/Inverted-right-triangle.js
@@ -0,0 +1,14 @@
+var prompt = require("prompt-sync")();
+function inverted_rightanglepattern() {
+  const n = Number(prompt("Enter the Number ... "));
+  for (let i = 1; i <= n; i++) {
+    // outer loop for rows
+    for (let j = 1; j <= n - i + 1; j++) {
+      // inner loop for columns
+      process.stdout.write("* ");
+    }
+    console.log();
+  }
+}
+
+inverted_rightanglepattern();
