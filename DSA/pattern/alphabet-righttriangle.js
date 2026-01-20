c181667e0e8367aaa088ba00b2988cfce7ae95f0
imraan011 <ishtikharkhandl@gmail.com>
2026-01-20 12:52:46 +0530

    Day 42 | Commit 8

---
+++ DSA/pattern/alphabet-righttriangle.js
@@ -0,0 +1,17 @@
+var prompt = require("prompt-sync")();
+function Alpha_righttripattern() {
+  const n = Number(prompt("Enter the Number ... "));
+  for (let i = 1; i <= n; i++) {
+    // outer loop for rows
+    temp = 65
+    for (let j = 1; j <= i; j++) {
+      // inner loop for columns
+      process.stdout.write(String.fromCharCode(temp) + " ");
+temp++
+    }
+
+    console.log();
+  }
+}
+
+Alpha_righttripattern();
