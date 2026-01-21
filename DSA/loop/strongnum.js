7429219d669d4eabfa152c6ffe41d8de3132c712
imraan011 <ishtikharkhandl@gmail.com>
2026-01-21 22:27:53 +0530

    Day 43 | Commit 1

--- DSA/loop/strongnum.js
+++ DSA/loop/strongnum.js
@@ -26,3 +26,28 @@ function isStrongNumber(n) {
     
 }
 isStrongNumber(145); // Output: "Yes" (1! + 4! + 5! = 145)
+
+
+
+// function strong_num() {
+//     var prompt = require("prompt-sync")()
+//     let n = Number(prompt("Enter the number .. "));
+  
+//     let copy = n;
+//     let sum = 0;
+//     while (n > 0) {
+//       let fact = 1;
+//       let rem = n % 10;
+//       for (let i = 1; i <= rem; i++) {
+//         fact = fact * i;
+//       }
+//       sum += fact;
+//       n = Math.floor(n / 10);
+//     }
+  
+//     if(copy==sum)console.log("strong");
+//     else console.log("not");
+//   }
+  
+//   strong_num();
+  
