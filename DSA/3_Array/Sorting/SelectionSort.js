278b6d089b398c27967f2c2103fac5488b295c5f
imraan011 <ishtikharkhandl@gmail.com>
2026-01-26 00:56:20 +0530

    Day 47 | Commit 4

---
+++ DSA/3_Array/Sorting/SelectionSort.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 57, 4, 6, 3, 2, 1];
+for (let i = 0; i < arr.length; i++) {
+  let small = i;
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[small] > arr[j]) {
+      small = j;
+    }
+  }
+  let temp = arr[i];
+  arr[i] = arr[small];
+  arr[small] = temp;
+}
+
+console.log(arr);
