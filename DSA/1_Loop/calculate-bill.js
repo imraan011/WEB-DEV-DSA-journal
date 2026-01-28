0bb09a3330b505a73942098d7c7b24f5b0caf845
imraan011 <ishtikharkhandl@gmail.com>
2026-01-28 22:38:20 +0530

    Day 48 | Commit 1

--- DSA/1_Loop/calculate-bill.js
+++ DSA/1_Loop/calculate-bill.js
@@ -1,22 +1,21 @@
 function calculateElectricityBill(unit) {
-    // Write your logic here
-    let amount =0;
-    if(unit>400){
-        amount=(unit-400)*13;
-        unit = 400;
-    }
-    if(unit>200  && unit<=400){
-        amount=amount + (unit-200)*8;
-        unit =200;
-    }
-    if(unit>100 && unit<=200){
-        amount= amount + (unit-100)*6;
-        unit =100;    
-        
-    }
-    amount=amount+ unit*4.2;
-  
-    return console.log(amount.toFixed(1))
+  // Write your logic here
+  let amount = 0;
+  if (unit > 400) {
+    amount = (unit - 400) * 13;
+    unit = 400;
+  }
+  if (unit > 200 && unit <= 400) {
+    amount = amount + (unit - 200) * 8;
+    unit = 200;
+  }
+  if (unit > 100 && unit <= 200) {
+    amount = amount + (unit - 100) * 6;
+    unit = 100;
+  }
+  amount = amount + unit * 4.2;
+
+  return console.log(amount.toFixed(1));
 }
 
-calculateElectricityBill(50);
+calculateElectricityBill(4000);
