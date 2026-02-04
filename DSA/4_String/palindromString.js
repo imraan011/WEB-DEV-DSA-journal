c9f3962d8d3aa708313d2440e41a2f9ed8daea98
imraan011 <ishtikharkhandl@gmail.com>
2026-02-04 23:44:18 +0530

    Day 51 | Commit 6 - more refined

--- DSA/4_String/palindromString.js
+++ DSA/4_String/palindromString.js
@@ -1,14 +1,15 @@
-const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")();
 
-let s = prompt("Enter the string")
-let ispalindrome = true
-let i = 0 , j=s.length-1
-while(i<j){
-    if(s[i]==s[j]) ispalindrome=true
-    else ispalindrome = false
-    i++
-    j--
+let s = prompt("Enter the string");
+let ispalindrome = true;
+let i = 0,
+  j = s.length - 1;
+while (i < j) {
+  if (s[i] !== s[j]) ispalindrome = false;
+
+  i++;
+  j--;
 }
 
-if(ispalindrome==true) console.log("String Is Palindrome");
+if (ispalindrome == true) console.log("String Is Palindrome");
 else console.log("String Is Not Palindrome");
