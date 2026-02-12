1b494c0dcf85c05569b73b18e625958a0ac2da98
imraan011 <ishtikharkhandl@gmail.com>
2026-02-12 23:41:15 +0530

    Day 54 | Commit 2

--- DSA/LeetCode-Questions-{Excluding Cohot}/7_33SearchinRotatedSortedarray.js
+++ DSA/LeetCode-Questions-{Excluding Cohot}/7_33SearchinRotatedSortedarray.js
@@ -3,3 +3,29 @@
  * @param {number} target
  * @return {number}
  */
+
+var search = function(arr, target) {
+    let left = 0, right  = arr.length-1
+    while(left<=right){
+        let mid = Math.floor((left+right)/2)
+
+        if(arr[mid]==target) return mid
+
+        if(arr[left]<=arr[mid]){
+            if(arr[left]<=target && target <=arr[mid]){
+                right = mid-1
+            }else{
+                left = mid+1
+            }
+        }else{
+            if(arr[mid]<=target && target<= arr[right]){
+                left = mid+1
+            }else{
+                right = mid-1
+            }
+
+        }
+    }
+   
+    return -1
+};
