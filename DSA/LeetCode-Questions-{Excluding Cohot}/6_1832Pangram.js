bbf1a75d97aa01dfcc4d6e52e78faa498cfc1c29
imraan011 <ishtikharkhandl@gmail.com>
2026-02-14 09:21:57 +0530

    Day 55 | Commit 1

--- DSA/LeetCode-Questions-{Excluding Cohot}/6_1832Pangram.js
+++ DSA/LeetCode-Questions-{Excluding Cohot}/6_1832Pangram.js
@@ -2,8 +2,7 @@
 var checkIfPangram = function(sentence) {
     let set =  new Set()
     for(let i = 0; i<sentence.length ; i++){
-        let char  = sentence.charAt(i)
-        set.add(char)        
+        set.add(sentence[i])        
     }
     return set.size == 26
 
