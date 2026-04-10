b803db447e4dbbb182145e5ae8f052520cdae8fe
imraan011 <ishtikharkhandl@gmail.com>
2026-04-11 00:02:13 +0530

    Refactor HTML structure, enhance CSS styles, and implement food rendering logic in the Snake game

--- Exercise24-snakeeee/script.js
+++ Exercise24-snakeeee/script.js
@@ -4,6 +4,12 @@ const box_width = 50;
 
 const colomns = Math.floor(board.clientWidth / box_width);
 const rows = Math.floor(board.clientHeight / box_hight);
+let intervalid = null;
+let directions = "right";
+let food = {
+  x: Math.floor(Math.random() * rows),
+  y: Math.floor(Math.random() * colomns),
+};
 
 const boardArray = [];
 snake = [
@@ -21,8 +27,6 @@ snake = [
   },
 ];
 
-const directions = "right";
-
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < colomns; j++) {
     const box = document.createElement("div");
@@ -34,11 +38,73 @@ for (let i = 0; i < rows; i++) {
 }
 
 function render() {
+  let head = null;
+  //food rendering logic
+
+  boardArray[`${food.x},${food.y}`].classList.add("food");
+
+  //snake rendering logic
+  if (directions === "left") {
+    head = {
+      x: snake[0].x,
+      y: snake[0].y - 1,
+    };
+  } else if (directions === "right") {
+    head = {
+      x: snake[0].x,
+      y: snake[0].y + 1,
+    };
+  } else if (directions === "up") {
+    head = {
+      x: snake[0].x - 1,
+      y: snake[0].y,
+    };
+  } else if (directions === "down") {
+    head = {
+      x: snake[0].x + 1,
+      y: snake[0].y,
+    };
+  }
+
+  if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= colomns) {
+    alert("Game Over");
+    clearInterval(intervalid);
+  }
+
+  if (food.x === head.x && food.y === head.y) {
+    boardArray[`${food.x},${food.y}`].classList.remove("food");
+    food = {
+      x: Math.floor(Math.random() * rows),
+      y: Math.floor(Math.random() * colomns),
+    };
+    boardArray[`${food.x},${food.y}`].classList.add("food");
+    snake.unshift(head);
+  }
+
+  snake.forEach((element) => {
+    box = boardArray[`${element.x},${element.y}`];
+    box.classList.remove("filled");
+  });
+  snake.unshift(head);
+  snake.pop();
+
   snake.forEach((element) => {
     boardArray[`${element.x},${element.y}`].classList.add("filled");
   });
 }
 
-setInterval(() => {
-  render();
-}, 300);
+// intervalid = setInterval(() => {
+//   render();
+// }, 300);
+
+addEventListener("keydown", (e) => {
+  if (e.key == "ArrowLeft") {
+    directions = "left";
+  } else if (e.key == "ArrowRight") {
+    directions = "right";
+  } else if (e.key == "ArrowUp") {
+    directions = "up";
+  } else if (e.key == "ArrowDown") {
+    directions = "down";
+  }
+});
